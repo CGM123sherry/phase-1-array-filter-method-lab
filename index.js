b3c8c95cfa3e8e5cebed6e54a3a3fc14b67d9969
CGM123sherry <g.christinemurithi@gmail.com>
2024-07-04 18:36:03 +0300

    completed a lab on arrayfilter method

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution here
+//matching drivers, case sensitive
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  //fuzzy match
+  function fuzzyMatch(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(query.toLowerCase()));
+}
+//match name
+function matchName(drivers, query){
+    return drivers.filter(driver => driver.name === query)
+}
